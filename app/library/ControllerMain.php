5deefcaac6aa12d9675355cb8bdd0b61e3bff83e
aldecirfonseca <aldecirfonseca@hotmail.com>
2022-03-20 10:57:04 -0300

    Aula dia 14-03-2022
    - Implementação de segurança nas pastas do framework para que não ocorra acessos diretos via navegador (utilizando o .htaccess), nas pastas system, app, assets, third.
    - Criado método loadView no controllerMain;
    - Criando as estruturas de views padrões cabecalho, rodapé, home e erros.

--- app/library/ControllerMain.php
+++ app/library/ControllerMain.php
@@ -8,4 +8,22 @@ class ControllerMain
     { 
         $this->dados = $dados;
     }
+
+    /**
+     * loadView - Carrega views
+     *
+     * @param string $nameView 
+     * @param array $dbDados 
+     * @return void
+     */
+    public function loadView($nameView, $dbDados = [])
+    {
+        $this->dbDados = $dbDados;
+
+        if (file_exists("app/view/" . $nameView . ".php")) {
+            require_once "app/view/" . $nameView . ".php";
+        } else {
+            require_once "app/view/comuns/erros.php";
+        }
+    }
 }
---
+++ app/views/comuns/erros.php
@@ -0,0 +1,5 @@
+<?= $this->loadView("comuns/cabecalho") ?>
+
+<p>Página não localizada...</p>
+
+<?= $this->loadView("comuns/rodape") ?>
